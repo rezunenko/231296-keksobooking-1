930b185e00989b4c72f26669236d81e1c0c3ae8a
rezunenko <tamerlan88111@yandex.ru>
2018-02-24 23:48:34 +0300

    handle request for static files

--- src/server.js
+++ src/server.js
@@ -1,30 +1,39 @@
 const http = require(`http`);
 const url = require(`url`);
 const fs = require(`fs`);
-const { promisify } = require(`util`);
+const {promisify} = require(`util`);
 const PORT = 3000;
 const HOSTNAME = `127.0.0.1`;
 const CONTENT_TYPES = {
   'css': `text/css`,
   'html': `text/html; charset=UTF-8`,
   'jpg': `image/jpeg`,
+  'png': `image/png`,
+  'svg': `image/svg+xml`,
   'ico': `image/x-icon`
 };
 
 const readFile = async (path, res) => {
-  const data = await promisify(fs.readFile);
-  res.setHeader(`content-type`, `text/plain`);
+  const data = await promisify(fs.readFile)(path);
+  const arr = path.split(`.`);
+  const contentType = arr[arr.length - 1];
+  if (!CONTENT_TYPES[contentType]) {
+    throw new Error(`Not resolver content-type`);
+  }
+  res.setHeader(`content-type`, CONTENT_TYPES[contentType]);
   res.setHeader(`content-length`, Buffer.byteLength(data));
   res.end(data);
 };
 
 const server = http.createServer((req, res) => {
-  const absolutePath = `${__dirname}/static/${url.parse(req.url).pathname}`;
-  console.log(absolutePath);
+  let path = url.parse(req.url).pathname;
+  if (path === `/`) {
+    path = `/index.html`;
+  }
+  const absolutePath = `${process.cwd()}/static${path}`;
   (async () => {
     try {
       const pathStat = await promisify(fs.stat)(absolutePath);
-      console.log(pathStat);
       if (pathStat.isDirectory()) {
         throw new Error(`Not found file`);
       }
