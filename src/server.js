c1ad8ec22d74b599e7804703543c09e340eb46fd
rezunenko <tamerlan88111@yandex.ru>
2018-02-25 18:59:41 +0300

    add body-parser and multer

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 const {generateElements} = require(`./generator`);
 const express = require(`express`);
+const bodyParser = require(`body-parser`);
+const multer = require(`multer`);
 const app = express();
 const PORT = 3000;
 const HOSTNAME = `localhost`;
 
 app.use(express.static(`static`));
+app.use(bodyParser.json());
+
+const upload = multer({storage: multer.memoryStorage()});
 
 const offers = generateElements(5);
 
@@ -17,6 +22,11 @@ app.get(`/api/offers/:date`, (req, res) => {
 
 app.get(`/api/offers`, (req, res) => {
   res.send(offers);
+});
+
+app.post(`/api/offers`, upload.none(), (req, res) => {
+  // TODO сделать корректную обработку запроса
+  res.send(req.body);
 });
 
 module.exports = {
