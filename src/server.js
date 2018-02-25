62eb12b698271b9b7a1b20f52a05bf4d6126ab63
rezunenko <tamerlan88111@yandex.ru>
2018-02-25 18:48:29 +0300

    add /api/offers and /api/offers/:date methods

--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
+const {generateElements} = require(`./generator`);
 const express = require(`express`);
 const app = express();
 const PORT = 3000;
 const HOSTNAME = `localhost`;
 
 app.use(express.static(`static`));
+
+const offers = generateElements(5);
+
+app.get(`/api/offers/:date`, (req, res) => {
+  const date = +req.params[`date`];
+  const filteredData = offers.data.find((item) => item.date === date);
+
+  res.send(filteredData);
+});
+
+app.get(`/api/offers`, (req, res) => {
+  res.send(offers);
+});
 
 module.exports = {
   name: `server`,
