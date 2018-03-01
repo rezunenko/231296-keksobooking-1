81edb82991892f2cb787f63cd9944c2e74a88193
rezunenko <tamerlan88111@yandex.ru>
2018-03-02 00:15:01 +0300

    parse query string when get /api/offers

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const {generateElements} = require(`./generator`);
+const {getElements} = require(`./generator`);
 const express = require(`express`);
 const bodyParser = require(`body-parser`);
 const multer = require(`multer`);
@@ -6,12 +6,12 @@ const app = express();
 const PORT = 3000;
 const HOSTNAME = `localhost`;
 const upload = multer({storage: multer.memoryStorage()});
-const offers = generateElements(5);
 
 app.use(express.static(`static`));
 app.use(bodyParser.json());
 
 app.get(`/api/offers/:date`, (req, res) => {
+  const offers = getElements(5);
   const date = +req.params[`date`];
   const filteredData = offers.data.find((item) => item.date === date);
 
@@ -19,10 +19,13 @@ app.get(`/api/offers/:date`, (req, res) => {
 });
 
 app.get(`/api/offers`, (req, res) => {
+  const {skip, limit} = req.query;
+  const offers = getElements(5, +skip, +limit);
   res.send(offers);
 });
 
 app.get(`/api/offers/:date/avatar`, (req, res) => {
+  const offers = getElements(5);
   const date = +req.params[`date`];
   const filteredData = offers.data.find((item) => item.date === date);
   if (!filteredData) {
