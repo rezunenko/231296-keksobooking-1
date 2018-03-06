e233f02041ea916ba89e174e5ce6c0490e916308
rezunenko <tamerlan88111@yandex.ru>
2018-03-06 23:52:59 +0300

    require dotenv

---
+++ dotend.js
@@ -0,0 +1,10 @@
+require(`dotenv`).config();
+
+console.log(`Server address ${process.end.SERVER_HOST}`);
+console.log(`Port ${process.end.SERVER_PORT}`);
+console.log(`Log level ${process.end.SERVER_LOG_LEVEL}`);
+
+console.log(`Db host address ${process.end.DB_HOST}`);
+console.log(`Db username ${process.end.DB_USER}`);
+console.log(`Db password ${process.end.DB_PASSWORD}`);
+
